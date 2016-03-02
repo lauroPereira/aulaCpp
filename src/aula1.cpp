565a4692e66929e437f695c7c905ba094974e88b
lauro pereira <lauro.s.periera@gmail.com>
2016-03-02 09:45:11 -0300

    commit aula 2.

--- src/aula1.cpp
+++ src/aula1.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <iomanip>
+//Namespaces
 using namespace std;
+namespace x{
+	string e = "Hello", f = "World", g = "!!!";
+}
 
 int main() {
-	cout << "Hello World!!!" << endl; // prints !!!Hello World!!!
+	//Declaração de variáveis
+	string a = "Hello", b = "World", c = "!!!";
+
+	//Cast de variáveis
+		//static_cast<char>(c);/*Conversão simples de tipos primários*/
+		//dynamic_cast<char>(c);/*Conversão de tipos de objetos*/
+		//reinterpret_cast<int*>(d);/*Realiza o cast mantendo o valor binario interno da variavel.*/
+		//const_cast<char>(c);/*Usado para tornar uma constante em variavel*/
+
+	cout <<  a << " " << x::f << c << c << endl;
+
+	int d;
+
+	cin >> d;
+	cout << "left:"			<< left 	<< setw(3) 	<< d 	<< endl;
+	cout << "right:"		<< right 	<< setw(3) 	<< d 	<< endl;
+	cout << "decimal:"		<< dec 		<< d 		<< endl;
+	cout << "octal:"		<< oct 		<< d 		<< endl;
+	cout << "hexadecimal:"	<< hex 		<< d 		<< endl;
+
 	return 0;
 }
